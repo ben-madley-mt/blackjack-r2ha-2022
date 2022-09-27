e089fa78ea318969c3e30c18f5d8f3e9d8ea6a0d
Ben Madley <ben.madley@madetech.com>
2022-09-27 15:55:50 +0100

    Extract cardsAsString.

--- src/test/java/com/r2ha/blackjack/HandDisplayTest.java
+++ src/test/java/com/r2ha/blackjack/HandDisplayTest.java
@@ -19,4 +19,15 @@ public class HandDisplayTest {
                 .isEqualTo("\u001B[31m┌─────────┐\u001B[1B\u001B[11D│A        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♥    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        A│\u001B[1B\u001B[11D└─────────┘");
     }
 
+    @Test
+    void displayHandAsCorrectString() throws Exception {
+        List<Card> cards = List.of(
+                new Card(Suit.HEARTS, Rank.ACE),
+                new Card(Suit.CLUBS, Rank.JACK));
+        Hand hand = new Hand(cards);
+
+        assertThat(hand.cardsAsString())
+                .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘[6A[1C[30m┌─────────┐[1B[11D│J        │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│        J│[1B[11D└─────────┘");
+    }
+
 }
