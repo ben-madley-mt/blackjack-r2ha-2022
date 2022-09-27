113cc74975e21533000735d2b3bb34d85cf5bdb1
Ben Madley <ben.madley@madetech.com>
2022-09-27 15:51:39 +0100

    Add DisplayHand method.

---
+++ src/test/java/com/r2ha/blackjack/HandDisplayTest.java
@@ -0,0 +1,22 @@
+package com.r2ha.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HandDisplayTest {
+
+    @Test
+    void displayFaceUpCardReturnsAsCorrectString() throws Exception {
+        List<Card> cards = List.of(
+                new Card(Suit.HEARTS, Rank.ACE),
+                new Card(Suit.CLUBS, Rank.JACK));
+        Hand hand = new Hand(cards);
+
+        assertThat(ConsoleHand.displayFaceUpCard(hand))
+                .isEqualTo("\u001B[31m┌─────────┐\u001B[1B\u001B[11D│A        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♥    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        A│\u001B[1B\u001B[11D└─────────┘");
+    }
+
+}
